674de3057317b4088a12a0caf87276a0587255f5
Amrutha A <160649644+Amruthaa14@users.noreply.github.com>
2025-06-11 21:09:11 +0530

    Create revString.java

---
+++ src/revString.java
@@ -0,0 +1,17 @@
+import java.util.;
+public class revString
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      String str=sc.next();
+      String rev="";
+      for(int i=str.length()-1;i<=1;i++)
+        {
+          rev+=str.charAt(i);
+        }
+      System.out.print("rev");
+    }
+  }
+}
+          
